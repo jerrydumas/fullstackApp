575245969c302eca466739318398c748bac29be9
jerrydumas <dumas.jerry@gmail.com>
2025-04-02 14:04:52 -0400

    daily first update

--- backend/server.js
+++ backend/server.js
@@ -6,38 +6,43 @@ const app = express();
 
 app.use(express.json())
 
-// Connection URL
-const url = process.env.MONGODB_CONNECT;
-const client = new MongoClient(url);
-
-// Database Name
-const dbName = 'AmazingMernApp';
-
-async function main() {
-	
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('animals');
-  return 'done.';
-}
-
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
-
 app.get('/', (req, res) => {
   res.send('Hello World');
 })
 
 app.get('/api/animals', async (req,res)=>{
-const findResult = await collection.find({}).toArray();
-console.log('Found documents =>', findResult);
+   await  listDatabases(client);
+// const findResult = await collection.find({}).toArray();
+// console.log('Found documents =>', findResult);
 })
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
 
+//connect to mongodb
+const uri = process.env.MONGODB_CONNECT
+const client = new MongoClient(uri)
+
+
+async function connect() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    // Make the appropriate DB calls
+   
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+  }
+}
+
+connect();
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
